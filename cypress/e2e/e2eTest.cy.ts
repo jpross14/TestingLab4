0a193c9fdb971cc8d5832b97dbd76162d5ded1be
Jm-altar <124422953+Jm-altar@users.noreply.github.com>
2025-05-16 08:38:14 +0800

    Update e2eTest.cy.ts

--- cypress/e2e/e2eTest.cy.ts
+++ cypress/e2e/e2eTest.cy.ts
@@ -1,5 +1,5 @@
 
-describe('WiseWay ToDo E2E', () => {
+describe('WiseWay ToDo List E2E', () => {
 
   const todo1 = "Buy groceries"
   const todo2 = "Pick up laundry"
@@ -8,50 +8,35 @@ describe('WiseWay ToDo E2E', () => {
     cy.visit('http://localhost:3000/')
   });
 
-  it('Visit the site, add Todos, Mark them Complete and Deletes them', () => {
+  it('Add Todos, Mark them complete then Delete', () => {
     // Add todos
     cy.get('#task-input').clear().type(todo1);
     cy.get('button[type="submit"]').click();
 
     cy.get('#task-input').clear().type(todo2);
     cy.get('button[type="submit"]').click();
- 
 
+  
     // Check if todos exist
     cy.contains(todo1).should('exist');
     cy.contains(todo2).should('exist');
-   
 
+ 
     // Mark first todo complete
     cy.contains(todo1)
       .parent()
       .parent()
       .find('input[type="checkbox"]')
-      .check({ force: true });
+      .check({ force: true })
+    
 
     // Check if todo is marked complete
     cy.contains(todo1)
       .should('have.class', 'line-through');
 
 
-    // ---ADD TESTS HERE----
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+      // --ADD TESTS HERE---
   });
 
   //Clears DB after test
